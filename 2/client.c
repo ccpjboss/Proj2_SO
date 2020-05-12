fb073d018eb0e5d986c60701e4b4ee88e38e2998
Me <jpccastilho1999@gmail.com>
2020-05-12 11:24:20 +0100

    done

--- 2/client.c
+++ 2/client.c
@@ -6,8 +6,12 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <string.h>
+#include <float.h>
+#include <semaphore.h>
 
 #define SHRMEM "/mem_example"
+#define SEM_NAME "/sem_read"
+#define SEM_NAME2 "/sem_write"
 
 int main(int argc, char const *argv[])
 {
@@ -15,9 +19,19 @@ int main(int argc, char const *argv[])
     int shrmem_fd;
     double *shrmem;
 
+    /* SEMAPHORES */
+    sem_t *sem_id_read = sem_open(SEM_NAME, O_CREAT, 0666, 0);
+    sem_t *sem_id_write = sem_open(SEM_NAME2, O_CREAT, 0666, 1);
+
+    if (sem_id_read == SEM_FAILED || sem_id_write == SEM_FAILED)
+    {
+        perror("sem_open");
+        exit(1);
+    }
     /* Files */
     char *file_name = "input.asc";
     double value_read;
+    double final = DBL_MIN;
 
     FILE *file = fopen(file_name, "r");
     if (file == NULL)
@@ -27,7 +41,7 @@ int main(int argc, char const *argv[])
     }
 
     // Create the shared memory object
-    if ((shrmem_fd = shm_open(SHRMEM, O_RDWR | O_CREAT, 0)) == -1)
+    if ((shrmem_fd = shm_open(SHRMEM, O_RDWR | O_CREAT, 0666)) == -1)
     {
         perror("shm_open");
         exit(1);
@@ -37,7 +51,7 @@ int main(int argc, char const *argv[])
     ftruncate(shrmem_fd, sizeof(double));
 
     // Map the shared memory
-    if ((shrmem = mmap(NULL, sizeof(double), PROT_WRITE, MAP_SHARED, shrmem_fd, 0)) == MAP_FAILED)
+    if ((shrmem = mmap(NULL, sizeof(double), PROT_WRITE | PROT_READ, MAP_SHARED, shrmem_fd, 0)) == MAP_FAILED)
     {
         perror("mmap");
         exit(1);
@@ -47,10 +61,20 @@ int main(int argc, char const *argv[])
     while (!feof(file))
     {
         printf("%lf\n", value_read);
-        memcpy(shrmem,&value_read,sizeof(double));
+        sem_wait(sem_id_write);
+        memcpy(shrmem, &value_read, sizeof(double));
+        sem_post(sem_id_read);
         fscanf(file, "%lf", &value_read);
     }
 
+    sem_wait(sem_id_write);
+    memcpy(shrmem, &final, sizeof(final));
+    sem_post(sem_id_read);
+
+    shm_unlink(SHRMEM);
+    sem_unlink(SEM_NAME);
+    sem_unlink(SEM_NAME2);
     fclose(file);
+
     return 0;
 }
--- 2/server.c
+++ 2/server.c
@@ -4,18 +4,31 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <string.h>
+#include <float.h>
+#include <semaphore.h>
 
 #define SHRMEM "/mem_example"
+#define SEM_NAME "/sem_read"
+#define SEM_NAME2 "/sem_write"
 
 int main(int argc, char const *argv[])
 {
     /* SHARED MEMORY */
     int shrmem_fd;
-    double* shrmem;
-    double value_read;
+    double *shrmem;
+    double value_read = 0;
+    int c =0;
+    sem_t *sem_id_read = sem_open(SEM_NAME,O_CREAT,0666,0);
+    sem_t *sem_id_write = sem_open(SEM_NAME2,O_CREAT,0666,1);
+
+    if (sem_id_read == SEM_FAILED || sem_id_write == SEM_FAILED)
+    {
+        perror("sem_open");
+        exit(1);
+    }
 
     /* File */
-    char* file_name = "input.bin";
+    char *file_name = "input.bin";
     FILE *file = fopen(file_name, "wb");
     if (file == NULL)
     {
@@ -24,21 +37,62 @@ int main(int argc, char const *argv[])
     }
 
     // Create the shared memory object
-    if ((shrmem_fd=shm_open(SHRMEM,O_RDONLY | O_CREAT,0)) == -1)
+    if ((shrmem_fd = shm_open(SHRMEM, O_RDONLY, 0666)) == -1)
     {
         perror("shm_open");
         exit(1);
     }
 
     // Map the shared memory
-    if ((shrmem =mmap(NULL,sizeof(double), PROT_READ,MAP_SHARED,shrmem_fd,0)) == MAP_FAILED) 
+    if ((shrmem = mmap(NULL, sizeof(double), PROT_READ, MAP_SHARED, shrmem_fd, 0)) == MAP_FAILED)
     {
         perror("mmap");
         exit(1);
     }
-    memcpy(&value_read, shrmem, sizeof(double));
-    fwrite(&value_read,sizeof(double),1,file);
-    fclose(file);
 
+    while (1)
+    {
+        sem_wait(sem_id_read);
+        memcpy(&value_read, shrmem, sizeof(double));
+        sem_post(sem_id_write);
+        if (value_read == DBL_MIN)
+            break;
+        printf("%lf\n", value_read);
+        fwrite(&value_read, sizeof(double), 1, file);
+        c=c+1;
+    }
+    printf("DONE READING SHARED MEMORY!\n");
+    shm_unlink(SHRMEM);
+    sem_unlink(SEM_NAME);
+    sem_unlink(SEM_NAME2);
+    fclose(file);
+     
+    double array[c];
+    FILE* file2 = fopen(file_name,"rb");
+    FILE* file3 = fopen("output.asc","w");
+
+    if (file2 == NULL)
+    {
+        perror("file read");
+        exit(1);
+    }
+
+    if(file3 == NULL)
+    {
+        perror("file3");
+        exit(1);
+    }
+    char value_to_write [50];
+    fread(array,sizeof(double),c,file2) ;
+    fclose(file2);
+
+    for (int i = 0; i < c; i++)
+    {
+        array[i]=array[i]*4.0;
+        snprintf(value_to_write,50,"%lf \n",array[i]);
+        fprintf(file3,"%s",value_to_write);
+    }
+    fclose(file3);
+    
     return 0;
 }
